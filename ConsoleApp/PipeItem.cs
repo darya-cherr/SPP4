6f5a176b2c8e6c34b6ac27387c836fe15c73d730
darya-cherr <chernikovadarya67@gmail.com>
2022-01-26 04:16:04 +0300

    Added file's loading and reading, generating, writing tasks

---
+++ ConsoleApp/PipeItem.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp
+{
+    public class PipeItem
+    {
+        public string path;
+        public string code;
+
+        public PipeItem(string path)
+        {
+            this.path = path;
+        }
+        
+        public PipeItem(string path, string code)
+        {
+            this.path = path;
+            this.code = code;
+        }
+        
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -7,10 +12,52 @@ namespace ConsoleApp {
 
     class Program
     {
+        
+        private static async Task<PipeItem[]> Read(PipeItem[] items)
+        {
+            Console.WriteLine("Reading");
+            return await Task<PipeItem[]>.Factory.StartNew(() => items.Select(x => new PipeItem(x.path, File.ReadAllText(x.path))).ToArray());
+        }
+        
+        private static async Task<PipeItem[]> Generate(PipeItem[] items)
+        {
+            Console.WriteLine("Generating");
+            return;
+        }
+
+        private static void Write(PipeItem[] items)
+        {
+            Console.WriteLine("Writing");
+        }
+        
         static void Main(string[] args)
         {
             string path = @"C:\Users\Dasha_2\RiderProjects\SPP4\ConsoleApp\TestPrograms";
             string resultPath = @"C:\Users\Dasha_2\RiderProjects\SPP4\ConsoleApp\TestResult";
+            
+            List<string> pathes = new List<string>(Directory.GetFiles(path));
+            List<IEnumerable<PipeItem>> items = new List<IEnumerable<PipeItem>>();
+           
+            
+            for (int i = 0; i < pathes.Count; i += 2) 
+            {
+                items.Add(pathes.GetRange(i, Math.Min(2, pathes.Count - i)).Select(x => new PipeItem(x))); 
+            }
+
+            foreach (IEnumerable<PipeItem> item in items)
+            {
+                var read = new TransformBlock<PipeItem[], PipeItem[]>(Read);
+                var generate = new TransformBlock<PipeItem[], PipeItem[]>(Generate);
+                var write = new ActionBlock<PipeItem[]>(Write);
+                var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+                
+                read.LinkTo(generate, linkOptions);
+                generate.LinkTo(write, linkOptions);
+
+                read.Post(item.ToArray());
+                read.Complete();
+               
+            }
             
         }
     }
