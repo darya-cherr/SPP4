b7960b6257c81cc62d3a38cda8f5fdc1618192d7
darya-cherr <chernikovadarya67@gmail.com>
2022-01-26 04:38:56 +0300

    Create code analyzer

---
+++ TestGeneratorLib/CodeAnalyzer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace TestGeneratorLib
+{
+    public class CodeAnalyzer
+    {
+        public void Analyse(string code)
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(code);
+            CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+            var namespaceDecalaration = root.Members.OfType<NamespaceDeclarationSyntax>().First();
+            var classDeclaration = namespaceDecalaration.Members.OfType<ClassDeclarationSyntax>().First();
+            var methodDeclaration = classDeclaration.Members.OfType<MethodDeclarationSyntax>().Where(method => method.Modifiers.Where(modifier => modifier.Kind() == SyntaxKind.PublicKeyword).Any());
+            
+        }
+    }
+}
